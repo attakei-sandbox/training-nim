07617697b6d18c5e0ccd8f2feb1a6bf6215956eb
Kazuya Takei <myself@attakei.net>
2020-05-18 18:38:12 +0900

    Using closure like

--- webserver/staticserver.nim
+++ webserver/staticserver.nim
@@ -15,30 +15,32 @@ import strutils
 import asynchttpserver, asyncdispatch
 
 
-proc resp_any(req: Request) {.async.} =
+proc resp_any(base_dir: string): (proc(req: Request) {.async, gcsafe.}) =
   ## リクエストに応じて実行フォルダ配下のファイルをレスポンスする
-  let resource = normalizePath(os.getCurrentDir() & req.url.path)
   let mime = newMimetypes()
-  if not existsFile(resource):
-    # ファイルが存在しない場合は、404を返す
-    await req.respond(
-      Http404,
-      "Not Found",
-      newHttpHeaders([("Content-Type", "text/plain")])
-    )
-  else:
-    # ファイルが存在する場合は、拡張子からmimeを判定して中身をレスポンス
-    let f = open(resource, FileMode.fmRead)
-    var mime_type: string = mime.getMimetype(resource.splitFile()[2])
-    if mime_type.split("/")[0] == "text":
-      mime_type = mime_type & ";charset=utf-8"
-    await req.respond(
-      Http200,
-      f.readAll(),
-      newHttpHeaders([("Content-Type", mime_type)])
-    )
+  return proc(req: Request) {.async.} =
+    let resource = normalizePath(os.getCurrentDir() & req.url.path)
+    if not existsFile(resource):
+      # ファイルが存在しない場合は、404を返す
+      await req.respond(
+        Http404,
+        "Not Found",
+        newHttpHeaders([("Content-Type", "text/plain")])
+      )
+    else:
+      # ファイルが存在する場合は、拡張子からmimeを判定して中身をレスポンス
+      let f = open(resource, FileMode.fmRead)
+      var mime_type: string = mime.getMimetype(resource.splitFile()[2])
+      if mime_type.split("/")[0] == "text":
+        mime_type = mime_type & ";charset=utf-8"
+      await req.respond(
+        Http200,
+        f.readAll(),
+        newHttpHeaders([("Content-Type", mime_type)])
+      )
 
 
 if isMainModule:
   let server = newAsyncHttpServer()
-  waitFor server.serve(Port(8080), resp_any)
+  let cb = resp_any(os.getCurrentDir())
+  waitFor server.serve(Port(8080), cb)
